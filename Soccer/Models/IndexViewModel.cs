69fe60bc87e7b13e540e8ea67140356bfb9d139b
Vitaliy <vitaliypolyanskiy@gmail.com>
2025-03-15 13:25:13 +0200

    Add project files.

---
+++ Soccer/Models/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Soccer.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Players> Players { get; set; } = new List<Players>();
+        public SortViewModel SortViewModel { get; set; } = new SortViewModel(SortState.NameAsc);
+    }
+}
---
+++ Soccer/Models/SortViewModel.cs
@@ -0,0 +1,56 @@
+﻿namespace Soccer.Models
+{
+    public class SortViewModel
+    {
+        public SortState NameSort { get; set; } // значение для сортировки по имени
+        public SortState AgeSort { get; set; }    // значение для сортировки по возрасту
+        public SortState PositionSort { get; set; }    // значение для сортировки по позиции
+        public SortState TeamSort { get; set; }   // значение для сортировки по компании
+        public SortState Current { get; set; }     // значение свойства, выбранного для сортировки
+        public bool Up { get; set; }  // Сортировка по возрастанию или убыванию
+
+        public SortViewModel(SortState sortOrder)
+        {
+            // значения по умолчанию
+            NameSort = SortState.NameAsc;
+            AgeSort = SortState.AgeAsc;
+            PositionSort = SortState.PositionAsc;
+            TeamSort = SortState.TeamAsc;
+            Up = true;
+
+            if (sortOrder == SortState.AgeDesc || sortOrder == SortState.NameDesc
+                || sortOrder == SortState.PositionDesc || sortOrder == SortState.TeamDesc)
+            {
+                Up = false;
+            }
+
+            switch (sortOrder)
+            {
+                case SortState.NameDesc:
+                    Current = NameSort = SortState.NameAsc;
+                    break;
+                case SortState.AgeAsc:
+                    Current = AgeSort = SortState.AgeDesc;
+                    break;
+                case SortState.AgeDesc:
+                    Current = AgeSort = SortState.AgeAsc;
+                    break;
+                case SortState.PositionAsc:
+                    Current = PositionSort = SortState.PositionDesc;
+                    break;
+                case SortState.PositionDesc:
+                    Current = PositionSort = SortState.PositionAsc;
+                    break;
+                case SortState.TeamAsc:
+                    Current = TeamSort = SortState.TeamDesc;
+                    break;
+                case SortState.TeamDesc:
+                    Current = TeamSort = SortState.TeamAsc;
+                    break;
+                default:
+                    Current = NameSort = SortState.NameDesc;
+                    break;
+            }
+        }
+    }
+}
